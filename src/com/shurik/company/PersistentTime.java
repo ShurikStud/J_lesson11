9bad14866e305c0ef54cc7f36af3542e999942ff
note-5i\shurik <abashev.alexandr@gmail.com>
2017-11-06 19:30:42 +0200

    + реализация PersistentTime

--- src/com/shurik/company/PersistentTime.java
+++ src/com/shurik/company/PersistentTime.java
@@ -1,4 +1,20 @@
 package com.shurik.company;
 
-public class PersistentTime {
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+
+public class PersistentTime implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private Date datetime;
+
+    public PersistentTime() {
+        datetime    = Calendar.getInstance().getTime();
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
 }
